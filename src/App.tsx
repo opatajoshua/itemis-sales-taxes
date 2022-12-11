e787ef5c9b3c00f21a27e0a081b0345c8ae2ba70
Opata Joshua <opatajoshua@gmail.com>
2022-12-11 20:35:47 +0100

    fix: updated app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { TrackView } from "./TrackView";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-screen bg-gray-100 h-screen p-4">
+      <h1 className="text-xl font-bold">Itemis Coding challenge</h1>
+      <h2 className="h">Joshua Opata</h2>
+      <TrackView />
     </div>
   );
 }
